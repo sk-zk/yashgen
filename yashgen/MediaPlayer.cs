fcb8ece1a96b6ef55858076950177e43e247c4f4
sk-zk <@>
2019-05-17 19:48:38 +0200

    Init

---
+++ yashgen/MediaPlayer.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yashgen
+{
+    /// <summary>
+    /// Interop class for UnityMediaPlayer.dll; copy-pasted from ILSpy ¯\_(ツ)_/¯
+    /// </summary>
+    class MediaPlayer : IDisposable
+    {
+        private bool disposed;
+        public float[] fft;
+        private GCHandle fftHandle;
+        private static MediaPlayer instance;
+
+        public MediaPlayer()
+        {
+
+            if (instance != null)
+            {
+                //Debug.LogError("MediaPlayer.cs DOH: tried to create a second singleton");
+            }
+            else
+            {
+                cppInit();
+                instance = this;
+                this.fft = new float[0x200];
+                this.fftHandle = GCHandle.Alloc(this.fft, GCHandleType.Pinned);
+            }
+        }
+
+        [DllImport("UnityMediaPlayer", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void cppDispose();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int cppGetBytesPerSecond();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern float cppGetDuration(string filePath);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int cppGetFFT512(IntPtr fft);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int cppGetFFT512KISS(IntPtr fft);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr cppGetMusicFolder();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern float cppGetPosition();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppGetPrescanWholeSongSums(IntPtr fft, int count);
+        [DllImport("UnityMediaPlayer", CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppInit();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppPause();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppPlay(string filePath);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern int cppPrescanWholeSong(string filePath);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppResume();
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppSetPosition(float secs);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern void cppSetVolume(float fVol);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppStartPrescan(string filePath);
+        [DllImport("UnityMediaPlayer", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+        private static extern bool cppStop();
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    this.fftHandle.Free();
+                }
+                cppDispose();
+                this.disposed = true;
+            }
+        }
+
+        ~MediaPlayer()
+        {
+            this.Dispose(false);
+        }
+
+        public int GetBytesPerSecond()
+        {
+            return cppGetBytesPerSecond();
+        }
+
+        public float GetDuration(string filePath)
+        {
+            return cppGetDuration(filePath);
+        }
+
+        public int GetFFT512()
+        {
+            return cppGetFFT512(this.fftHandle.AddrOfPinnedObject());
+        }
+
+        public int GetFFT512KISS()
+        {
+            return cppGetFFT512KISS(this.fftHandle.AddrOfPinnedObject());
+        }
+
+        public static string GetMusicFolder()
+        {
+            return Marshal.PtrToStringUni(cppGetMusicFolder());
+        }
+
+        public float GetPosition()
+        {
+            return cppGetPosition();
+        }
+
+        public bool GetPrescanWholeSongSums(float[] sums, int count)
+        {
+            GCHandle handle = GCHandle.Alloc(sums, GCHandleType.Pinned);
+            bool flag = cppGetPrescanWholeSongSums(handle.AddrOfPinnedObject(), count);
+            handle.Free();
+            return flag;
+        }
+
+        public void Pause()
+        {
+            cppPause();
+        }
+
+        public void PlaySongFile(string filePath)
+        {
+            cppPlay(filePath);
+        }
+
+        public int PrescanWholeSong(string filePath)
+        {
+            return cppPrescanWholeSong(filePath);
+        }
+
+        public void Reset()
+        {
+            cppDispose();
+            cppInit();
+        }
+
+        public void Resume()
+        {
+            cppResume();
+        }
+
+        public bool SetPosition(float secs)
+        {
+            return cppSetPosition(secs);
+        }
+
+        public void SetVolume(float fVol)
+        {
+            cppSetVolume(fVol);
+        }
+
+        public void StartPrescan2(string filePath)
+        {
+            cppStartPrescan(filePath);
+        }
+
+        public void Stop()
+        {
+            cppStop();
+        }
+
+        public static MediaPlayer Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    new MediaPlayer();
+                }
+                return instance;
+            }
+        }
+    }
+
+
+}
---
+++ yashgen/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace yashgen
+{
+    class Program
+    {
+        static SongLoader songLoader = new SongLoader();
+        static void Main(string[] args)
+        {
+            var id = args[0];
+            var path = args.Length > 1 ? args[1] : "";
+
+            try
+            {
+                if (IsYoutubeId(id))
+                {
+                    ProcessVideo(id, path);
+                }             
+                else
+                {
+                    Console.Error.WriteLine("\"{0}\" doesn't appear to be a valid ID\n", id);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Something went wrong:");
+                Console.Error.WriteLine(ex.ToString());
+                Environment.Exit(1);
+            }
+
+            #if DEBUG
+                Console.ReadLine();
+            #endif
+        }
+
+        static bool IsYoutubeId(string input)
+        {
+            if (input.Length != 11) return false;
+            if (input.Contains("/") || input.Contains("\\")) return false;
+            if (File.Exists(input)) return false;
+            return true;
+        }
+
+        static void ProcessVideo(string videoId, string path)
+        {
+            CreateAndSaveYash(videoId, path);         
+            Console.WriteLine("Done\n");
+        }
+
+        static void CreateAndSaveYash(string videoId, string path)
+        {
+            Console.WriteLine("Processing {0} now", videoId);
+            Console.WriteLine("Downloading audio");
+            string ytAudioFile; 
+            try
+            {
+                ytAudioFile = YoutubeDl.CallYoutubeDl(videoId);
+            } 
+            catch (YoutubeDlException yex)
+            {
+                Console.Error.WriteLine("youtube-dl encountered an error:");
+                Console.Error.WriteLine(yex.Message);
+                return;
+            }
+
+            Console.WriteLine("Analyzing song");
+            TagLib.File file = TagLib.File.Create(ytAudioFile);
+            float duration = (float)file.Properties.Duration.TotalSeconds;
+            file.Dispose();
+            List<float> sums = songLoader.DecodeSongSums(ytAudioFile, duration);
+
+            Console.WriteLine("Saving yash");
+            try
+            {
+                var filename = $"youtube_{videoId}.yash";
+                SaveYash(sums, duration, Path.Combine(path, filename));
+            }
+            catch (IOException ioex)
+            {
+                Console.Error.WriteLine("Something went wrong:");
+                Console.Error.WriteLine(ioex.ToString());
+            }
+        }
+
+        static void SaveYash(List<float> sums, float duration, string path)
+        {
+            const int HEADER_SIZE = 3 * 4;
+            var sumBytes = new byte[HEADER_SIZE + (sums.Count() * 4)];
+            Buffer.BlockCopy(BitConverter.GetBytes(1), 0, sumBytes, 0, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(duration), 0, sumBytes, 1 * 4, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(sums.Count), 0, sumBytes, 2 * 4, 4);
+            Buffer.BlockCopy(sums.ToArray(), 0, sumBytes, 3 * 4, sums.Count * 4);
+
+            File.WriteAllBytes(path, sumBytes);
+        }
+
+    }
+}
